8dc89304a9363cc11e37955e634395a704c3c5b5
Affaan <maffaanahmed@gmail.com>
2024-08-31 19:26:48 +0500

    Update dashboard with latest changes

--- src/App.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import Router from "./router/Router";
-import publicRoutes from "./router/routes/publicRoutes";
-
-export default function App() {
-  const [allRoutes, setAllRoutes] = useState([...publicRoutes]);
-  return <Router allRoutes={allRoutes} />;
-}
